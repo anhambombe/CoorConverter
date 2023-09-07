c5876b1cf93e83bf1abe69930041383053e17eee
anhambombe <84500027+anhambombe@users.noreply.github.com>
2023-09-08 01:36:13 +0200

    Update coord_convert0.py

--- coord_convert0.py
+++ coord_convert0.py
@@ -133,8 +133,8 @@ if uploaded_file:
 hide_st_style ="""
 <style>
 #MainMenu {visibility: hidden;}
-#footer {visibility: hidden;}
-#header {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
 <style>
 """
 st.markdown(hide_st_style,unsafe_allow_html=True)
